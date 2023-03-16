18d4f06d1abfee9d07894d1566c51655439d428e
kanggaro <tugassmstr2@gmail.com>
2023-03-16 22:55:49 +0700

    session 6 - authenticate and authorizate

--- BlazingPizza.Shared/Order.cs
+++ BlazingPizza.Shared/Order.cs
@@ -28,5 +28,6 @@ public class Order
 [JsonSerializable(typeof(Pizza))]
 [JsonSerializable(typeof(List<PizzaSpecial>))]
 [JsonSerializable(typeof(List<Topping>))]
+[JsonSerializable(typeof(Topping))]
 [JsonSerializable(typeof(Dictionary<string, string>))]
 public partial class OrderContext : JsonSerializerContext { }
